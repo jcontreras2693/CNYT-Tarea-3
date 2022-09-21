1f9c81961753f93c683a56ee8f8f3dda7fa78e55
jcontreras2693 <judacobe2693@gmail.com>
2022-09-20 20:39:15 -0500

    first commit

---
+++ LibSimulCuantico.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+from qiskit.visualization import plot_histogram
+# Plot a histogram
+def plothistogram():
+    counts = {'1':0.25, '2':0.2, '3':0.2,
+            '4':.35}
+    plot_histogram(counts)
+    plt.show()
+    return
+
+def accmatvec(mat,vec):
+    """accion de una matriz sobre un vector, puede ser de enteros o complejos.
+    """
+    resp=[]
+    for i in range(len(mat)):
+        x=mat[i]
+        for j in range(len(x)):
+            y=(valmultvecrealcomp(mat[i],vec))
+        resp.append(y)
+    return resp
+
+def valmultvecrealcomp(arr1,arr2):
+    """calcula el producto de un vector por otro, puede ser de enteros o de numeros complejos.
+    """
+    sumai=(0,0)
+    sumatoria=0
+    x=False
+    for i in range(len(arr1)):
+        try:
+            sumai=suma(sumai,producto(arr1[i],arr2[i]))
+            x=True
+        except TypeError:
+            sumatoria+=arr1[i]*arr2[i]
+    if x:
+        return sumai
+    return sumatoria
+
+def canicas(mat,vec,clicks):
+    """se ingresa como parametro la matriz, el vector y la cantidad de clicks
+    """
+    for k in range(int(clicks)):
+        vec=accmatvec(mat,vec)
+    return vec
+
+def transmat(mat):
+    """traspone una matriz
+    """
+    for i in range(len(mat)):
+        x=mat[i]
+        try:
+            for j in range(len(x)):
+                if i<=j:
+                    temp=mat[i][j]
+                    mat[i][j]=mat[j][i]
+                    mat[j][i]=temp
+        except TypeError:
+            return mat
+    return mat
+
+def multrendijas(mat,vec,clicks):
+    """se ingresa como parametro la matriz, el vector y la cantidad de clicks, da como resultado un vector, pueden usarse enteros o numeros complejos.
+    """
+    trans=transmat(mat)
+    for i in range(len(mat)):
+        if sum(trans[i])!=1:
+            return False
+    for j in range(int(clicks)):
+        vec = accmatvec(mat, vec)
+    return vec
+
+def suma(a, b):
+    """retorna la suma entre dos numeros complejos"""
+    return (a[0] + b[0]), (a[1] + b[1])
+
+def producto(a,b):
+    """retorna el producto entre dos numeros complejos"""
+    if a==(0,0) and b==(0,0):
+        return a
+    elif a[0]==b[0] and a[1]==(b[1]*-1):
+        return (a[0]**2)+(a[1]**2),0
+    return ((a[0]*b[0])-(a[1]*b[1])),((a[0]*b[1])+(a[1]*b[0]))
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Libreria Números Complejos
+#### Esta libreria contiene 3 funciones, la primera resuleva el primer punto, la segunda resuelve el segundo y tercer punto y la tercera resuleve el cuarto punto
+#### Las funciones son:
+- canicas()
+- multrendijas()
+- plothistogram()
+## Hecho Con
+- [PyCharm](https://www.jetbrains.com/es-es/pycharm/) - IDE de Python usado
+## Autores
+- **Juan Contreras** - [*jcontreras2693*](https://github.com/jcontreras2693)
+## Agradecimientos
+- <https://gist.github.com/PurpleBooth/109311bb0361f32d87a2>
+- <https://youtu.be/SWYqp7iY_Tc>
+- <https://youtu.be/y6XdzBNC0_0>
